a4c1a6f6e84e1a0e2179a10faa241feda406064d
Vard175 <galustyanvarduhi@gmail.com>
2020-12-14 05:31:25 -0800

    add

---
+++ KnightPlatformer/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+        public Transform pos1, pos2;
+        public float speed = 2.5f;
+        public Transform startPos;
+        Vector3 nextPos;
+    private void Start()
+    {
+        nextPos = startPos.position;
+    }
+    private void FixedUpdate()
+    {
+        if (transform.position == pos1.position)
+            nextPos = pos2.position;
+        if (transform.position == pos2.position)
+            nextPos = pos1.position;
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
+    }
+}
+
+/*  public Vector3 finishPos=new Vector3(95f,5.49f,0f); 
+  public float speed = 0.5f;
+  private Vector3 startPos;
+  private float trackPercent = 0;
+  private int direction = 1; 
+void Start()
+{
+  startPos = transform.position;
+}
+void Update()
+{
+  trackPercent += direction * speed * Time.deltaTime;
+  float x = (finishPos.x - startPos.x) * trackPercent + startPos.x;
+  float y = (finishPos.y - startPos.y) * trackPercent + startPos.y;
+  transform.position = new Vector3(x, y, startPos.z);
+  if ((direction == 1 && trackPercent > .9f) || (direction == -1 && trackPercent < .1f))
+  {
+      direction *= -1;
+  }
+}*/
--- KnightPlatformer/Assets/Scripts/PlayerController.cs
+++ KnightPlatformer/Assets/Scripts/PlayerController.cs
@@ -9,13 +9,12 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheckPoint;
     public LayerMask groundLayer;
     //public float froundCheckRadius;
-    private bool isTouchingGound;
-
-
+    Collider2D isTouchingGound;
+   
     private Rigidbody2D _body;
     private Animator _anim;
     private CircleCollider2D _circle;
-    
+
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
@@ -24,7 +23,7 @@ public class PlayerController : MonoBehaviour
         _circle = GetComponent<CircleCollider2D>();
     }
 
-    
+
     void Update()
     {
         //movement rght left
@@ -32,17 +31,40 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
 
+        //jumping with a ground check
         //float radius = _circle.radius; GroundCheckRadius
-        isTouchingGound = Physics2D.OverlapCircle(groundCheckPoint.position, _circle.radius,groundLayer);
-        //jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isTouchingGound)
+        isTouchingGound = Physics2D.OverlapCircle(groundCheckPoint.position, _circle.radius, groundLayer);
+        bool grounded = false;
+        if (isTouchingGound != null)
+        {
+            grounded = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-          //  _anim.SetBool("IsJumping", true);
         }
 
+        //connecting player with moving platform
+     
+    /*  WHY DON'T YOU WORK??????
+     *MovingPlatform platform=null;
+        if (isTouchingGound!=null)
+        {
+            platform = isTouchingGound.GetComponent<MovingPlatform>();
+        } 
+        if (platform != null)
+        {
+            transform.parent = platform.transform;
+            // Sets "newParent" as the new parent of the child GameObject.
+            transform.SetParent(platform.transform);
+        }
+        else
+        {
+            transform.parent = null;
+        }*/
+      
         //animations
-        _anim.SetBool("IsJumping", !isTouchingGound);
+            _anim.SetBool("IsJumping", !grounded);
         _anim.SetFloat("speed", Mathf.Abs(deltaX));
         //_anim.SetBool("IsJumping", false);
 
@@ -50,7 +72,19 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
         }
- 
+
     }
-    
+    //connecting moving platform with player(parenting)
+   private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "MovingPlatform")
+        {
+            transform.parent = collision.gameObject.transform;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.parent = null;
+    }
+
 }
